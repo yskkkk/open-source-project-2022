69236ffc4ef824911a6a390ca751828b8d8af0b0
youngsookim(ys-oss330) <samron3@naver.com>
2022-03-30 16:31:11 +0900

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello 
+{
+        public static void main(String args[])
+		{
+		
+		AddOne ao = new AddOne();
+		int y = ao.addOne(10);
+
+		System.out.println("y="+y);
+
+		/*
+		for(int=0;i<xx.length;i++)
+			System.out.println(xx[i]);
+		*/
+		}
+}
